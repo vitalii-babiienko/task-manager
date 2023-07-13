54bb5080e34007df240602ee25acee51a52b0734
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-07-13 11:27:30 +0300

    Implement CRUD for the Position model

---
+++ manager/urls.py
@@ -0,0 +1,53 @@
+from django.urls import path
+
+from manager.views import (
+    IndexView,
+    PositionListView,
+    PositionCreateView,
+    PositionUpdateView,
+    PositionDeleteView,
+    WorkerListView,
+    WorkerDetailView,
+    WorkerCreateView,
+    WorkerPositionUpdateView,
+    WorkerDeleteView,
+    TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
+    TaskListView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
+
+urlpatterns = [
+    path(
+        "",
+        IndexView.as_view(),
+        name="index",
+    ),
+    path(
+        "positions/",
+        PositionListView.as_view(),
+        name="position-list",
+    ),
+    path(
+        "positions/create/",
+        PositionCreateView.as_view(),
+        name="position-create",
+    ),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
+]
+
+app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+from django.db.models import QuerySet
 
-# Create your views here.
+from manager.forms import (
+    PositionSearchForm,
+    WorkerSearchForm,
+    WorkerCreationForm,
+    WorkerPositionUpdateForm,
+    TaskTypeSearchForm,
+    TaskSearchForm,
+    TaskForm,
+)
+from manager.models import (
+    Task,
+    TaskType,
+    Position,
+    Worker,
+)
+
+
+class IndexView(generic.TemplateView):
+    template_name = "manager/index.html"
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+
+        context["number_of_workers"] = get_user_model().objects.count()
+        context["number_of_tasks"] = Task.objects.count()
+        context["number_of_task_types"] = TaskType.objects.count()
+
+        return context
+
+
+class PositionListView(LoginRequiredMixin, generic.ListView):
+    model = Position
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+
+        context["search_form"] = PositionSearchForm(
+            initial={
+                "name": name,
+            }
+        )
+
+        return context
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Position.objects.all()
+
+        form = PositionSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"],
+            )
+
+        return queryset
+
+
+class PositionCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("manager:position-list")
+
+
+class PositionUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("manager:position-list")
+
+
+class PositionDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Position
+    success_url = reverse_lazy("manager:position-list")
